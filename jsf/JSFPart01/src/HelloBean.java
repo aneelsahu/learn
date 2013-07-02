05202b6caef47a9df3c0ed0dbb351fe06760f4db
aneelsahu <aneelsahu@gmail.com>
2013-07-02 13:17:07 -0300

    added sample jsf project part 1

---
+++ jsf/JSFPart01/src/HelloBean.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+	
+	public String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
